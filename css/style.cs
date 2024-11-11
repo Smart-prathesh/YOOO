424197d709c658a9b0cc3c53b4145a6b7f2024df
Prathesh Kumar A <128119232+Smart-prathesh@users.noreply.github.com>
2024-11-11 22:07:12 +0530

    DIGITAL MARKETING PROJECT

---
+++ css/style.cs
@@ -0,0 +1,61 @@
+   body {
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+        background-color: #f9f9f9;
+    }
+
+    /* Navbar Styling */
+    .navbar {
+        padding: 10px;
+        position: sticky;
+        top: 0;
+        z-index: 1000;
+        background-color: #ffffff;
+    }
+    .navbar-brand {
+        font-size: 24px;
+        color: blue;
+        font-weight: bold;
+        font-family: 'Arial Black', sans-serif;
+        text-shadow: 1px 1px 5px rgba(0, 0, 0, 0.2);
+    }
+
+    /* Header Brand Hover Style */
+    .navbar-brand:hover {
+        color: darkblue;
+        text-decoration: underline;
+    }
+
+    /* Navbar Link Hover Effect */
+    .nav-link {
+        color: black;
+        font-size: 16px;
+        transition: color 0.3s ease;
+    }
+    .nav-link:hover {
+        color: blue;
+        font-weight: bold;
+        text-decoration: underline;
+    }
+
+    /* News Section Styling */
+    .news h3 {
+        color: #ff0000;
+    }
+    .news {
+        font-family: Arial, Helvetica, sans-serif;
+        width: 100%;
+        max-width: 1300px;
+        margin: 30px auto;
+        padding: 20px;
+        background-color: #ffffff;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+        border-radius: 20px;
+        border: 5px solid #4872d8;
+    }
+    .carousel-inner img {
+        height: 500px;
+        object-fit: cover;
+    }
+    
